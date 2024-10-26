a77188e9234ec955dac8ce4054a24d3c4f21c51c
David Perry <125750519+dperry83@users.noreply.github.com>
2024-10-26 13:30:17 -0400

    initial header component built

---
+++ layout_ideas/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <PageHeader msg=""/>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import PageHeader from './components/PageHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    PageHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+}
+</style>
---
+++ layout_ideas/src/components/PageHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="header">
+    <img alt="David Perry Photography logo" src="../assets/DP_logo.jpg" id="logo">
+    <h1> {{ msg }} </h1>
+    <div id="navigation">
+      <h3 class="about">About</h3>
+      <h3 class="portfolio">Portfolio</h3>
+      <h3 class="contact">Contact</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+export default {
+    name: 'PageHeader',
+    props: {
+      msg: String
+    }
+  };
+
+</script>
+
+<style scoped>
+  #logo {
+    max-width: 300px;
+    background-color: grey;
+    flex: 1;
+  }
+  h1 {
+    background-color: grey;
+  }
+  #navigation {
+    display: flex;
+    vertical-align: bottom;
+    align-items: end;
+    justify-content: end;
+    background-color: lightgrey;
+    flex: 2;
+  }
+  #navigation h3 {
+    margin: 20px;
+  }
+  #header {
+    display: flex;
+    /* flex-wrap: wrap; */
+    align-content: end;
+    background-color: gold;
+    padding: 15px;
+  }
+
+</style>
