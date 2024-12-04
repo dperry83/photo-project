6429e1ae653c34be8fcbfa7f7172b3dc7e51159f
David Perry <125750519+dperry83@users.noreply.github.com>
2024-12-04 17:03:15 -0500

    user table created

---
+++ persistence/DB_script.sql
@@ -0,0 +1,8 @@
+-- CREATE DATABASE project_database;
+
+-- CREATE TABLE Users (
+-- 	userID INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
+--     name VARCHAR(100),
+--     email VARCHAR(100),
+--     PRIMARY KEY (userID)
+-- );
