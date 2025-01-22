2f68926cd28d7629491f1f5d96d60f15064d1baa
David Perry <125750519+dperry83@users.noreply.github.com>
2025-01-22 07:47:09 -0500

    NOW creating new sessions with Postman.  Issue appeared to be variable scope

--- PhotoServer/app/controllers/sessions_controller.rb
+++ PhotoServer/app/controllers/sessions_controller.rb
@@ -9,8 +9,8 @@ class SessionsController < ApplicationController
   def create
     # if (user = User.authenticate_by(params.permit(:email_address, :password)))
     if (@user = User.authenticate_by(session_params))
-      start_new_session_for user
-      redirect_to after_authentication_url, alert: "You've successfully logged in, #{user.name}"
+      start_new_session_for @user
+      redirect_to after_authentication_url, alert: "You've successfully logged in, #{@user.name}"
     else
       Rails.logger.info("Authentication failed for email: #{params[:email_address]}")
       redirect_to new_session_path, alert: "Try another email address or password."
