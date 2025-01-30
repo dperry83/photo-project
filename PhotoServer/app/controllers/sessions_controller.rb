61c37215228a1c23dab059639fbc87a912a2d199
David Perry <125750519+dperry83@users.noreply.github.com>
2025-01-30 07:49:57 -0500

    branched to revert to Rails native authentication function thru view

--- PhotoServer/app/controllers/sessions_controller.rb
+++ PhotoServer/app/controllers/sessions_controller.rb
@@ -7,19 +7,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # if (user = User.authenticate_by(params.permit(:email_address, :password)))
-    if (@user = User.authenticate_by(session_params))
-      start_new_session_for @user
-      redirect_to after_authentication_url, alert: "You've successfully logged in, #{@user.name}"
+    if user = User.authenticate_by(params.permit(:email_address, :password))
+    # if (@user = User.authenticate_by(session_params))
+      start_new_session_for user
+      redirect_to after_authentication_url, alert: "You've successfully logged in, #{user.name}"
     else
       Rails.logger.info("Authentication failed for email: #{params[:email_address]}")
       redirect_to new_session_path, alert: "Try another email address or password."
     end
   end
 
-  def session_params
-    params.require(:user).permit(:email_address, :password)
-  end
+  # def session_params
+  #   params.require(:user).permit(:email_address, :password)
+  # end
   def destroy
     terminate_session
     redirect_to new_session_path
