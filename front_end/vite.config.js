976a98cbf8cbebb699971233d9183cfe5774bd1d
dperry83 <125750519+dperry83@users.noreply.github.com>
2025-04-15 16:54:25 -0400

    continuing to implement frontend auth

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import eslintPlugin from 'vite-plugin-eslint';
+// import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
-    plugins: [vue(), eslintPlugin()],
+    plugins: [vue()],
     server: {
         proxy: {
             '/api': {
