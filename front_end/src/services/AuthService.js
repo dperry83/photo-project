c8b9c3ace8594ceddaed47a997eda69ba3aeb4e2
David Perry <125750519+dperry83@users.noreply.github.com>
2025-05-01 08:21:48 -0400

    brought AuthService login() back to home base.  next step: implement async login().

--- front_end/src/services/AuthService.js
+++ front_end/src/services/AuthService.js
@@ -1,25 +1,15 @@
 import axios from 'axios'
-import { useAuthStore } from '../stores/auth.js'
+// import { useAuthStore } from '../stores/auth.js'
 
 export default { 
 
-  async login(email, password) {
-    const auth = useAuthStore()
-
-    try {
-      console.log("sending request to /session/new")
-      const response = await axios.post(
-        '/session/new', 
-        { email, password },
-        { headers: { 'Content-Type': 'application/json'} }
-      )
-      
-      console.log("received response: ", response)
-      auth.setUser(response.data)
-      return response
-    } catch (error) {
-        console.error("AuthService.login failed: ", error)
-        throw error
-    }
+  login(email, password) {
+    return axios.post('/session/new', {
+      email_address: email,
+      password: password
+      }, {
+        headers: { 'Content-Type': 'application/json'}
+      } 
+    )  
   }
 }
